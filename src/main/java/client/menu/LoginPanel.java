64d67efe613c2b59bc072978e96813c537e698b6
alstonlo <40709307+alstonlo@users.noreply.github.com>
2019-01-17 10:38:06 -0500

    Color pallette

--- src/main/java/client/menu/LoginPanel.java
+++ src/main/java/client/menu/LoginPanel.java
@@ -1,6 +1,7 @@
 package client.menu;
 
 import client.Window;
+import client.utilities.Pallette;
 import client.utilities.ThreadPool;
 import client.utilities.Utils;
 import protocol.AuthenticateProtocol;
@@ -31,7 +32,6 @@ import java.awt.image.BufferedImage;
  */
 class LoginPanel extends DropDownPanel {
 
-    private final Color OUTLINE_COLOR = new Color(60, 51, 28);
     private BufferedImage loginDrape = Utils.loadScaledImage("resources/menu/login drape.png");
 
     private MenuPanel menuPanel;
@@ -169,11 +169,11 @@ class LoginPanel extends DropDownPanel {
         String defaultText = field.getText();
 
         field.setSize(Utils.scale(400), Utils.scale(90));
-        field.setForeground(OUTLINE_COLOR);
+        field.setForeground(Pallette.OUTLINE_COLOR);
         field.setBackground(new Color(247, 195, 210));
         field.setFont(Utils.loadFont("resources/mon.otf", Utils.scale(40)));
 
-        Border outline = BorderFactory.createLineBorder(OUTLINE_COLOR, Utils.scale(3), true);
+        Border outline = BorderFactory.createLineBorder(Pallette.OUTLINE_COLOR, Utils.scale(3), true);
         int padSize = Utils.scale(10);
         Border padding = BorderFactory.createEmptyBorder(0, padSize, 0, padSize);
         field.setBorder(BorderFactory.createCompoundBorder(outline, padding));
---
+++ src/main/java/client/utilities/Pallette.java
@@ -0,0 +1,9 @@
+package client.utilities;
+
+import java.awt.Color;
+
+public class Pallette {
+
+    public static final Color OUTLINE_COLOR = new Color(60, 51, 28);
+
+}
