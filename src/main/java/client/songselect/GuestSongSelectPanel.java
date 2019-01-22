9f604d48bf734694ddfc1bef051161e9bcb5b0d0
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2019-01-22 11:23:40 -0500

    commented

--- src/main/java/client/songselect/GuestSongSelectPanel.java
+++ src/main/java/client/songselect/GuestSongSelectPanel.java
@@ -2,8 +2,19 @@ package client.songselect;
 
 import client.Window;
 
+/**
+ * The SongSelectionPanel for the guest who does not choose the song
+ *
+ * @Author Alston Lo
+ * last updated 01/21/2019
+ */
 public class GuestSongSelectPanel extends SongSelectPanel{
 
+    /**
+     * Constructs a SongSelectPanel for the guest player (the one who cannot choose the song)
+     * @param window the Window object to which this panel belongs and is drawn upon
+     * @param opponent the opponent's username
+     */
     public GuestSongSelectPanel(Window window, String opponent) {
         super(window, opponent);
     }
--- src/main/java/client/songselect/HostSongSelectPanel.java
+++ src/main/java/client/songselect/HostSongSelectPanel.java
@@ -2,8 +2,19 @@ package client.songselect;
 
 import client.Window;
 
+/**
+ * The SongSelectionPanel for the host who will choose the song
+ *
+ * @Author Alston Lo
+ * last updated 01/21/2019
+ */
 public class HostSongSelectPanel extends SongSelectPanel {
 
+    /**
+     * Constructs a SongSelectPanel for the host player (the one who can choose the song)
+     * @param window the Window object to which this panel belongs and is drawn upon
+     * @param opponent the opponent's username
+     */
     public HostSongSelectPanel(Window window, String opponent) {
         super(window, opponent);
     }
